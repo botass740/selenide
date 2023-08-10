e762cdf1dd96589c18df07901c10fbfb4a76ac5b
Max test <botass740@gmail.com>
2023-08-10 22:40:52 +0300

    Update HW3

--- src/test/java/ru/netology/selenid/cardDeliveryTest.java
+++ src/test/java/ru/netology/selenid/cardDeliveryTest.java
@@ -3,6 +3,7 @@ import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -22,15 +23,16 @@ public class cardDeliveryTest {
     void shouldFillingCardForm () {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Краноярск");
-        String currentData = generateDate(4, "dd.MM.yyyy");
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] input").sendKeys(currentData);
+        String currentDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input")
+                .sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(currentDate);
         $("[data-test-id='name'] input").setValue("Романов Максим Иванович");
         $("[data-test-id='phone'] input").setValue("+79787574471");
         $("[data-test-id='agreement']").click();
         $("[button.button]").click();
-        $("[.notification__content]")
+        $("[data-test-id='notification']")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentData));
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
     }
 }
