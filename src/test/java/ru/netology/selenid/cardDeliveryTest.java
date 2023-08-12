d6f3205a0b56d354f074ef5e780954c278fa0178
Max test <botass740@gmail.com>
2023-08-12 20:48:27 +0300

    Update HW4

--- src/test/java/ru/netology/selenid/cardDeliveryTest.java
+++ src/test/java/ru/netology/selenid/cardDeliveryTest.java
@@ -13,7 +13,7 @@ import static com.codeborne.selenide.Selenide.open;
 import static org.openqa.selenium.remote.tracing.EventAttribute.setValue;
 
 
-public class cardDeliveryTest {
+public class CardDeliveryTest {
 
     private String generateDate(int addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
@@ -22,7 +22,7 @@ public class cardDeliveryTest {
     @Test
     void shouldFillingCardForm () {
         open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Краноярск");
+        $("[data-test-id='city'] input").setValue("Краcноярск");
         String currentDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input")
                 .sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
@@ -30,7 +30,7 @@ public class cardDeliveryTest {
         $("[data-test-id='name'] input").setValue("Романов Максим Иванович");
         $("[data-test-id='phone'] input").setValue("+79787574471");
         $("[data-test-id='agreement']").click();
-        $("[button.button]").click();
+        $("button.button").click();
         $("[data-test-id='notification']")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
